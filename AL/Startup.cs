fcc0d2dcff8199bc6c4b888cc8305ef23bd5c586
D <kritantablake@gmail.com>
2023-01-31 19:47:45 +0300

    last update on day 1

--- AL/Startup.cs
+++ AL/Startup.cs
@@ -46,7 +46,6 @@ namespace AL
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
